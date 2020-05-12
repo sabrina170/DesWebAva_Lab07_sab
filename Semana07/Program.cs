f4a2e666a7533a82ff7dd7662c656d4aac2be78d
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2020-05-11 19:46:26 -0500

    primer commit

---
+++ Semana07/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana07
+{
+    class Program
+    {
+        public static DataClasses1DataContext context = new DataClasses1DataContext();
+
+
+        static void Main(string[] args)
+        {
+           Grouping2Lmabda();
+            Console.Read();
+        }
+        static void IntroToLINQ()
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1}", num);
+            }
+        }
+        //Ejemplo 1 lambda
+        static void IntroToLINQLAMBDA()
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            var ParesLambda = numbers.Where(num => num % 2 == 0).ToArray();
+
+
+             foreach (int num in ParesLambda)
+            {
+                Console.Write("{0,1}", num);
+            }
+        }
+
+        static void DataSource()
+        {
+            var queryAllCustomers = from cust in context.clientes
+                                    select cust;
+
+            foreach(var item in queryAllCustomers)
+            {
+                Console.WriteLine(item.NombreCompañia);
+            }
+
+        }
+        //Ejemplo 2 lambda
+        static void DataSourcelambda()
+        {
+            var queryAllCustomersLam = context.clientes.ToList();
+
+                 foreach (var item in queryAllCustomersLam)
+            {
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+
+        static void Filtering()
+        {
+            var queryLondonCustomers = from cust in context.clientes
+                                       where cust.Ciudad == "Londres"
+                                       select cust;
+            foreach(var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.Ciudad);
+            }
+        }
+        // Ejemplo 3 Lambda
+        static void FilteringLambda()
+        {
+            var queryLondonCustomers = context.clientes.Where(cust => cust.Ciudad== "Londres").ToList();
+            foreach (var item in queryLondonCustomers)
+            {
+                Console.WriteLine(item.Ciudad);
+            }
+        }
+
+        static void Ordering()
+        {
+            var queryLondonCustomers3 = from cust in context.clientes
+                                        where cust.Ciudad == "Londres"
+                                        orderby cust.NombreCompañia ascending
+                                        select cust;
+            foreach (var item in queryLondonCustomers3)
+            {
+
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+        //Ejemplo 4 Lambda
+        static void OrderingLambda()
+        {
+            var queryLondonCustomers3 = context.clientes.Where(cust => cust.Ciudad == "Londre")
+                                        .OrderBy(cust => cust.NombreCompañia);
+            foreach (var item in queryLondonCustomers3)
+            {
+
+                Console.WriteLine(item.NombreCompañia);
+            }
+        }
+
+
+        static void Grouping()
+        {
+            var queryCustomersByCity = from cust in context.clientes
+                                        group cust by cust.Ciudad;
+
+            foreach (var customerGroup in queryCustomersByCity)
+            {
+                Console.WriteLine(customerGroup.Key);
+
+                foreach (clientes customer in customerGroup)
+                {
+                    Console.WriteLine(" {0}", customer.NombreCompañia);
+                }
+            }
+        }
+        //ejemplo 5 lambda
+        static void GroupingLmabda()
+        {
+            var queryCustomersByCity = context.clientes.GroupBy(cust => cust.Ciudad);
+
+            foreach (var customerGroup in queryCustomersByCity)
+            {
+                Console.WriteLine(customerGroup.Key);
+
+                foreach (clientes customer in customerGroup)
+                {
+                    Console.WriteLine(" {0}", customer.NombreCompañia);
+                }
+            }
+        }
+
+        static void Grouping2()
+        {
+            var custQuery = from cust in context.clientes
+                            group cust by cust.Ciudad into custGroup
+                            where custGroup.Count() > 2
+                            orderby custGroup.Key
+                            select custGroup;
+
+            foreach (var item in custQuery)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+        //Ejemplo 6 lambda
+        static void Grouping2Lmabda()
+        {
+            var custQuery = context.clientes.GroupBy(cust => cust.Ciudad)
+                .Where(custGroup => custGroup.Count() > 2)
+                .OrderBy(custGroup=>custGroup.Key);
+
+            foreach (var item in custQuery)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        static void Joining()
+        {
+            var innerJoinQuery = from cust in context.clientes
+                                join dist in context.Pedidos on cust.idCliente equals dist.IdCliente
+                                select new { CustomerName = cust.NombreCompañia, DistributorName = dist.PaisDestinatario };
+
+            foreach (var item in innerJoinQuery)
+            {
+                Console.WriteLine(item.CustomerName);
+            }
+        }
+        //Ejempplo 7 lambda
+        static void JoiningLambda()
+        {
+            var innerJoinQuery = context.clientes
+                .Join(context.Pedidos, cust => cust.idCliente, dist => dist.IdCliente, (a, b) => new {a.NombreCompañia,b.PaisDestinatario});
+
+            foreach (var item in innerJoinQuery)
+            {
+                Console.WriteLine($"Nombre : {item.NombreCompañia} y destinatario: {item.PaisDestinatario}");
+            }
+        }
+    }
+
+
+}
---
+++ Semana07/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Semana07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Semana07")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("82671804-be29-484e-a7d3-8bc1dda60108")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
